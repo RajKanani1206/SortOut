d8a34a9cdbafbcdaa2f52ae82acc42d0b4102fd5
Raj Kanani <rajkanani1206@gmail.com>
2022-07-13 20:28:51 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return <div>{/* <Header /> */}</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
