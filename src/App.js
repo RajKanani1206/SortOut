f220ebf18d8a1850e3d40c1080f02fb72ce99011
Raj Kanani <rajkanani1206@gmail.com>
2022-07-17 12:53:34 +0530

    Created random array function and added bubble sort algorithm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Algorithm from "./components/Constants/Algorithm";
 import { ElementsContext } from "./components/ElementsContext";
 import Header from "./components/Header";
 
 const App = () => {
+  const [array, setArray] = useState([]);
   const [arraySize, setArraySize] = useState(10);
-  const [algorithm, setAlgorithm] = useState(null);
+  const [algorithm, setAlgorithm] = useState();
+  const [trace, setTrace] = useState([]);
+
+  const colorKey = Algorithm[algorithm]?.colorKey;
+  const description = Algorithm[algorithm]?.description;
+
+  const createTrace = () => {
+    const numbers = [...array];
+    const sort = Algorithm[algorithm]?.algorithm;
+    if (sort) {
+      const trace = sort(numbers);
+      setTrace(trace);
+    }
+  };
+
+  const generateRandomArray = () => {
+    const randomArray = Array(arraySize)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * Math.floor(arraySize * 5)) + 1);
+    setTrace([]);
+    setArray(randomArray);
+  };
+
+  useEffect(() => {
+    generateRandomArray();
+  }, [arraySize, algorithm]);
+
+  useEffect(() => {
+    createTrace();
+  }, [array]);
 
   return (
-    <ElementsContext.Provider value={{ arraySize, setArraySize, algorithm, setAlgorithm }}>
+    <ElementsContext.Provider
+      value={{ array, setArray, arraySize, setArraySize, algorithm, setAlgorithm, trace, setTrace }}
+    >
       <div>
         <Header />
       </div>
